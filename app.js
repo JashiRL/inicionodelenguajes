f1f30f54d7e41bae0fccd8a0963cf5371728d899
JashiRL <j.robleslecona@ugto.mx>
2023-08-14 09:17:33 -0600

    creacion de un arreglo de frutas y como mostrarlas

---
+++ app.js
@@ -0,0 +1,14 @@
+//const es una variables que no cambia 
+// var es una variable que puede ir cambiandp
+// esta es la forma de definir una funcion con la arrow Function 
+//  const holamundo = (uno, dos) => {
+//  }
+//  
+
+//revisar en mis tiempos libres como realizar obejtos y manipularlos en prpgramacon
+
+let frutas = ['pera', 'fresa', 'manzana', 'mango', 'papaya']
+
+frutas.forEach(fruta => {
+    console.log('la fruta es:  ', fruta)
+})
